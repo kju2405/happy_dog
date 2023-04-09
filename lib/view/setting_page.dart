133e5afcf01a5025c4a0ee19d781f78ace2ec4e2
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-04-09 11:15:49 +0900

    Adding dog profile and person profile in setting page

--- lib/view/setting_page.dart
+++ lib/view/setting_page.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:happy_dog/add_image/add_image.dart';
+import 'package:happy_dog/config/palette.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -8,10 +13,187 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  void showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: Colors.white,
+          child: AddImage(),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Setting Page'),
+    return Scaffold(
+      backgroundColor: Palette.backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    showAlert(context);
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/ddangkong.jpeg'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '이름 : 땅콩',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '나이 : 10개월',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '성별 : 남',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '품종 : 말티푸',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            "특징",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            '사람을 좋아해요, 입질이 있어요, 강아지들과 잘 놀아요',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    showAlert(context);
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/person.jpeg'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '이름 : 김철수',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '나이 : 28',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '성별 : 남',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '산책 소요시간 : 30분',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            "산책 시간대 : 저녁 8시",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '직업 : 학생',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '조언 가능 분야 : 앱개발',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            '강아지에대해 궁금한점 : 배변교육이 잘 안돼요',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
