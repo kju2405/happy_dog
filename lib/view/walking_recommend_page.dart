7bd6b8aed88a0528669cd9d4903e1457285b129d
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-04-10 16:15:00 +0900

    Adding background at walking recommend page

--- lib/view/walking_recommend_page.dart
+++ lib/view/walking_recommend_page.dart
@@ -17,251 +17,263 @@ class _RecommendPageState extends State<RecommendPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            '오늘은 어디로 산책을 가볼까?',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("images/illustration12.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
             ),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-          Text(
-            '- 산책 시간 -',
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              Radio(
-                value: 1,
-                groupValue: _walkingTime,
-                onChanged: (value) {
-                  setState(() {
-                    _walkingTime = value!;
-                    print(_walkingTime);
-                  });
-                },
+            Text(
+              '오늘은 어디로 산책을 가볼까?',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
-              Text('30분 이하'),
-              Radio(
-                value: 2,
-                groupValue: _walkingTime,
-                onChanged: (value) {
-                  setState(
-                    () {
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              '- 산책 시간 -',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: _walkingTime,
+                  onChanged: (value) {
+                    setState(() {
                       _walkingTime = value!;
                       print(_walkingTime);
-                    },
-                  );
-                },
-              ),
-              Text('30분 ~ 1시간'),
-              Radio(
-                value: 3,
-                groupValue: _walkingTime,
-                onChanged: (value) {
-                  setState(
-                    () {
-                      _walkingTime = value!;
-                      print(_walkingTime);
-                    },
-                  );
-                },
-              ),
-              Text('1시간 ~ 1시간 30분'),
-            ],
-          ),
-          Row(
-            children: [
-              Radio(
-                value: 4,
-                groupValue: _walkingTime,
-                onChanged: (value) {
-                  setState(() {
-                    _walkingTime = value!;
-                    print(_walkingTime);
-                  });
-                },
-              ),
-              Text('1시간30분 ~ 2시간'),
-              Radio(
-                value: 5,
-                groupValue: _walkingTime,
-                onChanged: (value) {
-                  setState(
-                    () {
-                      _walkingTime = value!;
-                      print(_walkingTime);
-                    },
-                  );
-                },
-              ),
-              Text('2시간 이상'),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            '- 산책 유형 -',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                children: [
-                  Radio(
-                    value: 1,
-                    groupValue: _walkingType,
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          _walkingType = value!;
-                          print(_walkingType);
-                        },
-                      );
-                    },
-                  ),
-                  Text('걷기'),
-                ],
-              ),
-              Row(
-                children: [
-                  Radio(
-                    value: 2,
-                    groupValue: _walkingType,
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          _walkingType = value!;
-                          print(_walkingType);
-                        },
-                      );
-                    },
-                  ),
-                  Text('달리기'),
-                ],
-              ),
-              Row(
-                children: [
-                  Radio(
-                    value: 3,
-                    groupValue: _walkingType,
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          _walkingType = value!;
-                          print(_walkingType);
-                        },
-                      );
-                    },
-                  ),
-                  Text('등산'),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            '- 산책 난이도 -',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                children: [
-                  Radio(
-                    value: 1,
-                    groupValue: _walkingDifficultyLevel,
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          _walkingDifficultyLevel = value!;
-                          print(_walkingDifficultyLevel);
-                        },
-                      );
-                    },
-                  ),
-                  Text('1 ~ 3'),
-                ],
-              ),
-              Row(
-                children: [
-                  Radio(
-                    value: 2,
-                    groupValue: _walkingDifficultyLevel,
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          _walkingDifficultyLevel = value!;
-                          print(_walkingDifficultyLevel);
-                        },
-                      );
-                    },
-                  ),
-                  Text('4 ~ 7'),
-                ],
-              ),
-              Row(
-                children: [
-                  Radio(
-                    value: 3,
-                    groupValue: _walkingDifficultyLevel,
-                    onChanged: (value) {
-                      setState(
-                        () {
-                          _walkingDifficultyLevel = value!;
-                          print(_walkingDifficultyLevel);
-                        },
-                      );
-                    },
-                  ),
-                  Text('8 ~ 10'),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          ElevatedButton.icon(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return LookingRecommend();
+                    });
                   },
                 ),
-              );
-            },
-            icon: Icon(Icons.recommend),
-            label: Text('추천 경로 보기'),
-          )
-        ],
+                Text('30분 이하'),
+                Radio(
+                  value: 2,
+                  groupValue: _walkingTime,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _walkingTime = value!;
+                        print(_walkingTime);
+                      },
+                    );
+                  },
+                ),
+                Text('30분 ~ 1시간'),
+                Radio(
+                  value: 3,
+                  groupValue: _walkingTime,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _walkingTime = value!;
+                        print(_walkingTime);
+                      },
+                    );
+                  },
+                ),
+                Text('1시간 ~ 1시간 30분'),
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 4,
+                  groupValue: _walkingTime,
+                  onChanged: (value) {
+                    setState(() {
+                      _walkingTime = value!;
+                      print(_walkingTime);
+                    });
+                  },
+                ),
+                Text('1시간30분 ~ 2시간'),
+                Radio(
+                  value: 5,
+                  groupValue: _walkingTime,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _walkingTime = value!;
+                        print(_walkingTime);
+                      },
+                    );
+                  },
+                ),
+                Text('2시간 이상'),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '- 산책 유형 -',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  children: [
+                    Radio(
+                      value: 1,
+                      groupValue: _walkingType,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _walkingType = value!;
+                            print(_walkingType);
+                          },
+                        );
+                      },
+                    ),
+                    Text('걷기'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: 2,
+                      groupValue: _walkingType,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _walkingType = value!;
+                            print(_walkingType);
+                          },
+                        );
+                      },
+                    ),
+                    Text('달리기'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: 3,
+                      groupValue: _walkingType,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _walkingType = value!;
+                            print(_walkingType);
+                          },
+                        );
+                      },
+                    ),
+                    Text('등산'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '- 산책 난이도 -',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  children: [
+                    Radio(
+                      value: 1,
+                      groupValue: _walkingDifficultyLevel,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _walkingDifficultyLevel = value!;
+                            print(_walkingDifficultyLevel);
+                          },
+                        );
+                      },
+                    ),
+                    Text('1 ~ 3'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: 2,
+                      groupValue: _walkingDifficultyLevel,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _walkingDifficultyLevel = value!;
+                            print(_walkingDifficultyLevel);
+                          },
+                        );
+                      },
+                    ),
+                    Text('4 ~ 7'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: 3,
+                      groupValue: _walkingDifficultyLevel,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _walkingDifficultyLevel = value!;
+                            print(_walkingDifficultyLevel);
+                          },
+                        );
+                      },
+                    ),
+                    Text('8 ~ 10'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LookingRecommend();
+                    },
+                  ),
+                );
+              },
+              icon: Icon(Icons.recommend),
+              label: Text('추천 경로 보기'),
+            )
+          ],
+        ),
       ),
     );
   }
